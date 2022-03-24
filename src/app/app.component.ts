1816ed56ead04d5c33be2a372813eee4b38439b8
Rodolphe <rodolphebansard@yahoo.com>
2022-03-24 20:54:59 +0100

    rating component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'interactive-rating-component';
+
+  public rating = 0;
+  public isRated = false;
+  public numberOfRating = 5;
+  public counter = Array;
+
+  public rateService(value: number){
+    if(value === this.rating){
+      this.rating = 0;
+    }
+    else{
+      this.rating = value
+    }
+  }
+
+  public rate(){
+    this.isRated = true;
+  }
 }
